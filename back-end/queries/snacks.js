699985dc0a2bb507e280544b9420ef7cf2499e60
Deborah Campos <631care@gmail.com>
2022-02-11 10:32:52 -0500

    AllSnack ,oneSnack quieries completed

--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -1,3 +1,25 @@
 const db = require("../db/dbConfig.js");
 
-module.exports = {};
+const getAllSnacks = async () =>{
+    try{
+        const allSnacks = await db.any("SELECT * FROM snacks");
+        return allSnacks;
+    }catch(err){
+        return err;
+    }
+}
+const getOneSnack = async (id) =>{
+
+    try{
+        const snack = await db.one("SELECT * FROM snackss WHERE id=$1", id);
+        return snack;
+    }catch(err){
+        return err;
+    }
+}
+
+
+module.exports = {
+    getAllSnacks,
+    getOneSnack
+};
