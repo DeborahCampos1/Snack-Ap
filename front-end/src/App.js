3a27c816d596576d54cf4a311be1a6c9054cdb75
Charlie Moran <Carlosamoran18@gmail.com>
2022-02-11 17:24:05 -0500

    front end stuff

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+/* pages */
+import Details from "./Pages/Details";
+import Edit from "./Pages/Edit";
+import Home from "./Pages/Home";
+import New from "./Pages/New";
+import PageNF from "./Pages/PageNF";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <BrowserRouter>
+        <NavBar />
+        <main>
+          main
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/:id" element={<Details />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/edit" element={<Edit />} />
+            <Route path="*" element={<PageNF />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
