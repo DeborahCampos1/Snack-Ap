7d50bcc12143ae8bd8213ece9d0b116d3880b55c
Deborah Campos <631care@gmail.com>
2022-02-12 07:44:33 -0500

    line 1-3 on App.js updated to test HeartHealth.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,13 @@
+import HeartHealth from "./Components/HeartHealth";
+let condition = HeartHealth(false);
+// snack.is_healthy will be passed into the above function on /snacks/:id page once completed
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello, world!</h1>
+      {condition}
     </div>
   );
 }
