a036a5b5664dba7e02a36d5a030ab96c0602621c
Deborah Campos <631care@gmail.com>
2022-02-12 00:07:04 -0500

    confirmHealth & spellCheck added, all tests pass

--- back-end/confirmHealth.js
+++ back-end/confirmHealth.js
@@ -1,3 +1,25 @@
-const confirmHealth = (snack) => {};
+const confirmHealth = (snack) => {
+
+    if(typeof snack.protein !== "number" || typeof snack.fiber !== "number" || typeof snack.added_sugar !== "number"){
+        return null
+      } if(snack.added_sugar < 5 && (snack.protein >=5 || snack.fiber >=5)){
+        return true
+      } else if (snack.added_sugar < 5 && snack.fiber > 5){
+        return true
+      } else if (snack.added_sugar < 5 && snack.protein > 5){
+        return true
+      } else if (snack.added_sugar > 5 && snack.fiber > 5){
+        return false
+      } else if (snack.added_sugar > 5 && snack.protein > 5){
+        return false
+      }else if(snack.added_sugar > 5 && snack.protein >=5 && snack.fiber >=5){
+        return false
+      } else if(snack.added_sugar > 5 && snack.protein < 5 && snack.fiber < 5){
+        return false
+      } else if(snack.added_sugar < 5 && snack.protein < 5 && snack.fiber < 5){
+        return false
+      }    
+};
+
 
 module.exports = confirmHealth;
--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,16 +1,15 @@
 const express = require('express');
+const { is } = require('express/lib/request');
+const confirmHealth = require('../confirmHealth.js');
+const correctSnack = require('../spellCheck');
 const snacks = express.Router();
 
 const {getAllSnacks, getOneSnack, createSnack , deleteSnack, updateSnack} = require("../queries/snacks.js");
-
   
-
-
 snacks.get("/", async (req,res)=>{
 
     try{
         const allSnacks = await getAllSnacks();
-
         if(allSnacks.length){
             res.status(200).json({success: true, payload: allSnacks});
         } else {
@@ -20,13 +19,13 @@ snacks.get("/", async (req,res)=>{
         console.log(err)
     }
 });
+
 snacks.get("/:id", async (req,res)=>{
     const { id } = req.params;
     try{
         const snack = await getOneSnack(id);
-
         if(snack.id){
-            res.status(200).json({success: true, payload: snack});
+            res.status(200).json({success: true, payload: snack, });
         } else {
             res.status(404).json({success: false, payload: "Snack not found"});
         }
@@ -39,17 +38,7 @@ snacks.post("/", async (req,res)=>{
     const { body } = req;
     try{
         const postSnack = await createSnack(body);
-
         if(postSnack.id && !postSnack.image){
-              
-                const correctSnack = (postSnack)=>{
-                    let snackName = postSnack.name.split(' ');
-                
-                    for (let i = 0; i < snackName.length; i++ ){
-                    snackName[i] = snackName[i].length> 2 ? snackName[i][0].toUpperCase() + snackName[i].slice(1).toLowerCase() : snackName[i];
-                    }
-                    return snackName.join(' ');
-                }
             let correctedSnack = correctSnack(postSnack);
             res.status(200).json(
                 {
@@ -61,40 +50,35 @@ snacks.post("/", async (req,res)=>{
                         fiber: postSnack.fiber,
                         protein: postSnack.protein,
                         added_sugar: postSnack.added_sugar,
-                        is_healthy: postSnack.is_healthy,      
+                        is_healthy: postSnack.is_healthy,
                     }
             });
 
         } if(postSnack.id && postSnack.image){
+            let correctedSnack = correctSnack(postSnack)
+            res.status(200).json(
+                {
+                    success: true, 
+                    payload: {
+                        id: postSnack.id,
+                        name: correctedSnack,
+                        image: postSnack.image,
+                        fiber: postSnack.fiber,
+                        protein: postSnack.protein,
+                        added_sugar: postSnack.added_sugar,
+                        is_healthy: postSnack.is_healthy,      
+                    }
+            });
 
-            const correctSnack = (postSnack)=>{
-                let snackName = postSnack.name.split(' ');
-              
-                for (let i = 0; i < snackName.length; i++ ){
-                  snackName[i] = snackName[i].length> 2 ? snackName[i][0].toUpperCase() + snackName[i].slice(1).toLowerCase() : snackName[i];
-                }
-                return snackName.join(' ');
-              }
-
-              let correctedSnack = correctSnack(postSnack)
-              console.log("post test line 82 " + postSnack.name);
-
-            res.status(200).json({success: true, payload: {
-                id: postSnack.id,
-                name: correctedSnack,
-                image: postSnack.image,
-                fiber: postSnack.fiber,
-                protein: postSnack.protein,
-                added_sugar: postSnack.added_sugar,
-                is_healthy: postSnack.is_healthy,      
-            }})
         } else {
             res.status(404).json({success: false, payload: "Snack not found"})
         }
+
     }catch(err){
         console.log(err);
     }
 });
+
 snacks.delete("/:id", async (req,res)=>{
     const { id } = req.params;
     try{
@@ -108,10 +92,10 @@ snacks.delete("/:id", async (req,res)=>{
         console.log(err)
     }
 });
+
 snacks.put("/:id", async (req,res)=>{
     const { id } = req.params;
     const { body } = req;
-
     const updatedSnack = await updateSnack(id, body);
     if(updatedSnack.id){
         res.status(200).json(updatedSnack)
