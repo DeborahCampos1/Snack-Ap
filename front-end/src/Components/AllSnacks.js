2523c452abf2b6c7f2e174331fa3698324a69741
Deborah Campos <631care@gmail.com>
2022-02-13 02:27:27 -0500

    snacks & HearthHealth.js rendering on AllSnacks

--- front-end/src/Components/AllSnacks.js
+++ front-end/src/Components/AllSnacks.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import HeartHealth from "./HeartHealth";
 const API = process.env.REACT_APP_API_URL;
 
 function AllSnacks() {
@@ -10,15 +11,23 @@ function AllSnacks() {
     axios.get(`${API}/snacks`)
     .then((res)=>{
       console.log(res.data)
+      setSnacks(res.data.payload)
     }).catch((err)=>{
       console.log(err)
     })
   }, [])
-
+let snackList = snacks.map((snack)=>{
+  return (
+    <div>
+      {snack.name} <span>{HeartHealth(snack.is_healthy)}</span>
+      <li>{snack.protein}</li>
+    </div>
+  )
+})
     return (
       <div className="AllSnacks">
         <h1>Hello, AllSnacks!</h1>
-       
+        {snackList}
       </div>
     );
   }
